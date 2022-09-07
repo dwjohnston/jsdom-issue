5872dd8a465e4bd1225b80ae5b66b4bd20027636
David Johnston <djohnston@cydarm.com>
2022-09-07 13:00:16 +1000

    Update

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function App(props) {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <form onSubmit = {(e) => {
+          e.preventDefault(); 
+          props.onSubmit(e.target.foo.value)
+        }}>
+            <input type ="text" name ="foo"/>
+
+            <button type ="submit">submit</button>
+        </form>
+
       </header>
     </div>
   );
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,37 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from './App';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+  const mockFn = jest.fn();
+  render(<App onSubmit = {mockFn}/>);
+
+  const button = screen.getByRole("button", {name: "submit"}); 
+
+  userEvent.click(button);
+
+  expect(mockFn).toHaveBeenCalledWith('');
+
+});
+
+
+test.only('responds to text input', () => {
+
+  const mockFn = jest.fn();
+  render(<App onSubmit = {(data) => {
+
+    console.log("gets here");
+    mockFn(data); 
+  }}/>);
+
+  const text = screen.getByRole("textbox"); 
+  userEvent.type(text, "foobar");
+
+  const button = screen.getByRole("button", {name: "submit"}); 
+
+  userEvent.click(button);
+
+  expect(mockFn).toHaveBeenCalledWith('foobar');
+
 });
